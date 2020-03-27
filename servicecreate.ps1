5fa914e5196fed3d2baf727d3c87cf95cbb123ee
zakhar.malinovskiy <zakhar.malinovskiy@gmail.com>
2020-03-27 13:22:27 +0200

    fix installation scripts

--- servicecreate.ps1
+++
@@ -1,9 +0,0 @@
-# Run as administartor.
-# If your execution policy forbides this file execution of this file
-#   Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
-$pathtobin = "d:/uploadserver/"
-$pathtolog = "d:/logs/"
-$pathtoStorage = "d:/storage/"
-
-New-Service -Name uploader1 -BinaryPathName $pathtobin + "uploadserver.exe -log " +$pathtolog + "uploadserver.log -listenOn 192.168.2.4:64000 -config "+$pathtobin+" -root "+$pathtoStorage+" -asService" -DisplayName "uploader" -Description "holds backups" -StartupType Automatic
